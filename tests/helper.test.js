d6ad596a535559f43b68d9d4d0cfaffe95fa1355
nwaguvictor <leofizzy3@gmail.com>
2021-12-16 16:27:09 +0100

    feat: helper functions passing test

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -1,9 +1,6 @@
 'use strict';
 
-const axios = require('axios').default;
-const { getItemByAvailability, fetchData } = require('../src/utils/helper');
-
-jest.mock('axios');
+const { getItemByAvailability, findMany } = require('../src/utils/helper');
 
 describe('Helper Mathods', () => {
   describe('getItemByAvailability', () => {
@@ -21,21 +18,69 @@ describe('Helper Mathods', () => {
     });
   });
 
-  describe('fetchClinics', () => {
-    it('should resolve with an array of objects of clinics', async () => {
-      const response = [
-        {
-          data: {
-            name: 'Good Health Home',
-            stateName: 'Alaska',
-            availability: { from: '10:00', to: '19:30' },
-          },
-        },
+  describe('findMany', () => {
+    it('should return an array of all objects if query object is empty', () => {
+      const arrayOfObject = [{ name: 'a' }, { name: 'b' }];
+      const query = {};
+      const result = findMany(query, arrayOfObject);
+      expect(result).toEqual([{ name: 'a' }, { name: 'b' }]);
+    });
+    it('returns array of objects matching the given name AND state query options, otherwise, or empty array', () => {
+      const arrayOfObject = [
+        { name: 'a', stateCode: 'b' },
+        { clinicName: 'c', stateName: 'd' },
       ];
-      axios.get.mockResolvedValue(response);
-      // const url = 'https://storage.googleapis.com/scratchpay-code-challenge/dental-clinics.json';
-      const data = await fetchClinics('/url');
-      await expect(fetchClinics('/url')).toEqual(1);
+      const result = findMany({ name: 'a', stateCode: 'b' }, arrayOfObject);
+
+      expect(result).toEqual(expect.arrayContaining([{ name: 'a', stateCode: 'b' }]));
+      expect(findMany({ name: 'e', stateCode: 'f' }, arrayOfObject)).toEqual([]);
+    });
+    it('returns array of objects matching the given name AND availability query options, or empty array', () => {
+      const arrayOfObject = [
+        { name: 'a', availability: { from: '00:01', to: '00:01' } },
+        { name: 'b', availability: { from: '00:11', to: '00:11' } },
+      ];
+      const query = { name: 'a', availability: 'from:00:01,to:00:01' };
+      const result = findMany(query, arrayOfObject);
+
+      expect(result).toEqual(expect.arrayContaining([{ name: 'a', availability: { from: '00:01', to: '00:01' } }]));
+      expect(findMany({ name: 'a', availability: 'from:00:11,to:00:22' }, arrayOfObject)).toEqual([]);
+    });
+    it('returns array of objects matching the given state AND availability query options, or empty array', () => {
+      const arrayOfObject = [
+        { stateCode: 'a', availability: { from: '00:01', to: '00:01' } },
+        { stateName: 'b', opening: { from: '00:11', to: '00:11' } },
+      ];
+      const query = { state: 'a', availability: 'from:00:01,to:00:01' };
+      const result = findMany(query, arrayOfObject);
+      const expected = { stateCode: 'a', availability: { from: '00:01', to: '00:01' } };
+
+      expect(result).toEqual(expect.arrayContaining([expected]));
+      expect(findMany({ state: 'c', availability: 'from:00:01,to:00:01' }, arrayOfObject)).toEqual([]);
+    });
+    it('returns array of objects matching the given name query options, or empty array', () => {
+      const arrayOfObject = [{ name: 'a' }, { clinicName: 'b' }];
+      const result = findMany({ name: 'a' }, arrayOfObject);
+
+      expect(result).toEqual(expect.arrayContaining([{ name: 'a' }]));
+      expect(findMany({ name: 'c' }, arrayOfObject)).toEqual([]);
+    });
+    it('returns array of objects matching the given state query options, or empty array', () => {
+      const arrayOfObject = [{ stateCode: 'a' }, { stateName: 'b' }];
+      const result = findMany({ state: 'b' }, arrayOfObject);
+
+      expect(result).toEqual(expect.arrayContaining([{ stateName: 'b' }]));
+      expect(findMany({ state: 'c' }, arrayOfObject)).toEqual([]);
+    });
+    it('returns array of objects matching the given availability query options, or empty array', () => {
+      const arrayOfObject = [
+        { availability: { from: '00:01', to: '00:11' } },
+        { opening: { from: '00:01', to: '00:11' } },
+      ];
+      const result = findMany({ availability: 'from:00:01,to:00:11' }, arrayOfObject);
+
+      expect(result).toEqual(expect.arrayContaining([{ availability: { from: '00:01', to: '00:11' } }]));
+      expect(findMany({ opening: 'from:00:01,to:00:22' }, arrayOfObject)).toEqual([]);
     });
   });
 });
